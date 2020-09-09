0d776678061f687f9e9dc6d8d317d45b2f640801
AndreVTavares <andretavares3@gmail.com>
2020-09-09 11:14:00 -0300

    passing the api request to the landing page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import Routes from './routes';
-import api from './services/api';
 
-import './assets/styles/global.css'
+import './assets/styles/global.css';
 
 function App() {
-  const [apiResponse, setApiResponse] = useState([]);
-
-  useEffect(() => {
-    getApiRequest();
-  }, []);
-
-  const getApiRequest = () => {
-    api
-      .get(
-        'search/tv?api_key=e0ced1b1c697172ba3767280468b0fd8&page=1&query=My%20Mister&include_adult=false'
-      )
-      .then((response) => {
-        console.log(response.data);
-        response.data.results.map((result: any) => {
-          console.log(result.overview);
-          console.log(result.name);
-        });
-      });
-  };
-
-  return (
-      <Routes/>
-  );
+  return <Routes />;
 }
 
 export default App;
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import api from '../../services/api';
 
 const LandingPage = () => {
-  return <h1>Welcome to Asian Media</h1>;
+  const [title, setTitle] = useState('');
+
+  useEffect(() => {
+    getApiRequest();
+  }, []);
+
+  const getApiRequest = () => {
+    api
+      .get(
+        'search/tv?api_key=e0ced1b1c697172ba3767280468b0fd8&page=1&query=My%20Mister&include_adult=false'
+      )
+      .then((response) => {
+        console.log(response.data);
+        response.data.results.map((result: any) => {
+          console.log(result.overview);
+          setTitle(result.name);
+        });
+      });
+  };
+
+  return (
+    <>
+      <h1>Welcome to Asian Media</h1>
+      <p>the best dorama: {title}</p>
+    </>
+  );
 };
 
 export default LandingPage;
